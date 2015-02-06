a7fe67cddd6ab22fe9c8ba887e2b101dd554a582
Tommyluo <tluo12321@yahoo.com>
2015-02-06 14:10:37 -0800

    revised

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-var number;
+var fizz = 0;
+var buzz = 0;
+var fb = 0;
 
-for (number = 0; number <= 100; number++) {
-  console.log("Number " + number);
-  if(number % 3 === 0) {
-    console.log("Fizz");
-  } else if (number % 5 === 0);
-  console.log("Buzz");
-} else if (number % 3 ===0)  && (number % 5 === 0)  {
-  console.log("FizzBuzz");
-} else {
-  
-}
+function fizzBuzz() {
+
+  for (var number = 1; number <= 100; number++) {
+
+    if(number % 3 === 0 && number % 5 === 0)  {
+      console.log("FizzBuzz")
+      fb++;
+    } 
+    else if (number % 5 === 0) {
+      console.log("Buzz");
+      buzz++;
+    } 
+    else if (number % 3 === 0) {
+      console.log("Fizz");
+      fizz++;
+    } 
+    else {
+      console.log(number);
+    }
+    console.log("Fizz" + fizz);
+    console.log("Buzz" + buzz);
+    console.log("FizzBuzz" + fb);
+  }
